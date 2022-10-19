2187490bccfb3af937c1997b9dc1d4a7ca2e6f3f
ykazarinov <kazarinov.yanek@gmail.com>
2022-10-19 20:02:54 +0200

    001

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -6,10 +6,9 @@ import UserModel from '../models/User.js'
 export const register = async (req, res) => {
 
     try{
-
         
         const password = req.body.password  
-        const salt = await bcrypt.genSalt(10) // алгоритм шифрования
+        const salt = await bcrypt.genSalt(10) // алгоритм шифрования 
         const hash = await bcrypt.hash(password, salt) // зашифрованный пароль
         
         const doc = new UserModel({
