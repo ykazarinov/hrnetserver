2753fe9d79d539a5737506f64ac0a4cd82177f26
ykazarinov <kazarinov.yanek@gmail.com>
2022-10-21 11:57:13 +0200

    fix

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -45,29 +45,29 @@ export const register = async (req, res) => {
 export const login = async (req, res) => {
    
     try{
+        console.log('test')
+       
+        const user = await UserModel.findOne({email: req.body.email})
 
-       console.log('test')
-        // const user = await UserModel.findOne({email: req.body.email})
+        if(!user){
+            return res.status(404).json({message: 'User does not exist'})
+        }
+        const isValidPass  = await bcrypt.compare(req.body.password, user._doc.passwordHash)
+        if(!isValidPass){
+            return res.status(400).json({message: 'Wrong login or password'})
+        }
+        const token = jwt.sign({
+            _id: user._id
+        }, process.env.TOKEN_CODE, {
+            expiresIn: '30d'
+        })
 
-        // if(!user){
-        //     return res.status(404).json({message: 'User does not exist'})
-        // }
-        // const isValidPass  = await bcrypt.compare(req.body.password, user._doc.passwordHash)
-        // if(!isValidPass){
-        //     return res.status(400).json({message: 'Wrong login or password'})
-        // }
-        // const token = jwt.sign({
-        //     _id: user._id
-        // }, process.env.TOKEN_CODE, {
-        //     expiresIn: '30d'
-        // })
-
-        // const {passwordHash, ...userData} = user._doc
+        const {passwordHash, ...userData} = user._doc
         
-        // res.json({
-        //     ...userData,
-        //     token,
-        // })
+        res.json({
+            ...userData,
+            token,
+        })
     }
     catch(err){
         console.log(err)
